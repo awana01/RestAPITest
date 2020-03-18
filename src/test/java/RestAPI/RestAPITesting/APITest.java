0921b516174027107570a251041d19f9ff577af4
DESKTOP-IC5KQM6\Server <rajeevsinghawana@outlook.com>
2020-03-18 14:57:20 +0530

    first commit

---
+++ src/test/java/RestAPI/RestAPITesting/APITest.java
@@ -0,0 +1,42 @@
+package RestAPI.RestAPITesting;
+
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+
+public class APITest {
+	
+	String uri = "https://api.openweathermap.org";
+	
+	@Test()
+	public void TestWeatherByCityName() {
+		RestAssured.baseURI = uri;
+		
+		RequestSpecification httpRequest =  RestAssured.given();
+				                            
+		Response response = httpRequest.request(Method.GET, "/data/2.5/weather?q=Delhi&appid=eec65f8966686eb7c89609f36eb92cff");
+		
+		String responseBody =response.getBody().asString(); 
+		int status = response.getStatusCode();
+		String headers = response.headers().toString();
+		
+		System.out.println("=== Request Response =====");
+		System.out.println(responseBody);
+		System.out.println("=== Request Response =====");
+		System.out.println("Status Code: "+status);
+		System.out.println("=== Request Headers ====");
+		System.out.println(headers );
+	}
+    
+    @Test()
+    public void TestWeatherByCityAndCountry_Name() {
+    	
+    }
+
+    
+
+
+}
