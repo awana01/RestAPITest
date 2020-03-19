746ba905fc1cb3f212b513ad8b3554d7479f23d1
DESKTOP-IC5KQM6\Server <rajeevsinghawana@outlook.com>
2020-03-19 19:36:41 +0530

    commit 2

--- src/test/java/RestAPI/Asna/GETRequest/AsnaTest.java
+++ src/test/java/RestAPI/Asna/GETRequest/AsnaTest.java
@@ -1,5 +1,71 @@
 package RestAPI.Asna.GETRequest;
 
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.authentication.PreemptiveAuthProvider;
+import io.restassured.authentication.PreemptiveBasicAuthScheme;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 public class AsnaTest {
-  
+	
+	String uri = "https://app.asana.com";
+	String token ="0/38d75b5504a78662a2adb04410b9b366";
+    
+	@Test()
+	public void GetAsnaWorkSpaces() {
+        RestAssured.baseURI = uri;
+        
+        
+		
+		RequestSpecification httpRequest =  RestAssured.given().header("Authorization", "Bearer "+token);
+				                            
+		Response response = httpRequest.request(Method.GET, "/api/1.0/workspaces/");
+		
+		String responseBody =response.getBody().asString(); 
+		int status = response.getStatusCode();
+		
+//		String headers = response.headers().toString();
+//		
+		System.out.println("=== Request Response ===== ");
+		System.out.println(responseBody);
+		System.out.println("=== Request Response =====");
+		System.out.println("Status Code: "+status);
+//		System.out.println("=== Request Headers ====");
+//		System.out.println(headers );
+	}
+	
+	
+	@Test()
+	public void GetAsnaProjects() {
+        RestAssured.baseURI = uri;
+        
+        RequestSpecification httpRequest =  RestAssured.given().header("Authorization", "Bearer "+token);
+				                            
+		Response response = httpRequest.request(Method.GET, "/api/1.0/projects/");
+		
+		String responseBody =response.getBody().asString(); 
+		int status = response.getStatusCode();
+		
+//		String headers = response.headers().toString();
+//		
+		System.out.println("=== Request Response ===== ");
+		System.out.println(responseBody);
+		System.out.println("=== Request Response =====");
+		System.out.println("Status Code: "+status);
+//		System.out.println("=== Request Headers ====");
+//		System.out.println(headers );
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/test/java/RestAPI/RestAPITesting/APIGetTest.java
+++ src/test/java/RestAPI/RestAPITesting/APIGetTest.java
@@ -1,5 +1,6 @@
 package RestAPI.RestAPITesting;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
@@ -7,7 +8,7 @@ import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 
-public class APITest {
+public class APIGetTest {
 	
 	String uri = "https://api.openweathermap.org";
 	
@@ -31,12 +32,35 @@ public class APITest {
 		System.out.println(headers );
 	}
     
-    @Test()
-    public void TestWeatherByCityAndCountry_Name() {
-    	
+    @Test(dataProvider="CityNames")
+    public void TestWeatherByCityAndCountry_Name(String city) {
+RestAssured.baseURI = uri;
+		
+		RequestSpecification httpRequest =  RestAssured.given();
+				                            
+		Response response = httpRequest.request(Method.GET, "/data/2.5/weather?q="+city+"i&appid=eec65f8966686eb7c89609f36eb92cff");
+		
+		String responseBody =response.getBody().asString(); 
+		int status = response.getStatusCode();
+		String headers = response.headers().toString();
+		System.out.println("======="+city+"========");
+		System.out.println("=== Request Response ===== ");
+		System.out.println(responseBody);
+		System.out.println("=== Request Response =====");
+		System.out.println("Status Code: "+status);
+		System.out.println("=== Request Headers ====");
+		System.out.println(headers );
+		System.out.println("=========================");
     }
 
     
-
+    @DataProvider(name="CityNames")
+    public Object [][] cityNameProvider(){
+    	String[][] cities= { 
+    			             {"London"},
+    			             {"Delhi"}
+    			           };
+    	return cities;
+    }
 
 }
